989450c34d08920a3720fbb9bbea91b31d6d575a
Stuart Thompson <stuart@stuartthompson.net>
2024-04-19 14:09:09 -0700

    Extending subcommands for add, edit, list, remove

--- src/main.rs
+++ src/main.rs
@@ -17,27 +17,41 @@ enum Command {
     Add {
         /// The item to add
         item: String,
-        // 
+        /// The amount by which to increase the tally for this item 
         count: u32,
     },
     /// Edits an existing item
     Edit {
         /// The id of the item to edit
-        #[arg(short, long)]
         id: u32,
     },
     /// Lists all items
     List,
     /// Removes an item
-    Remove,
+    Remove {
+        /// The id of the item to remove
+        id: u32,
+    }
+}
+
+fn increment(item: String, count: u32) {
+    println!("Incrementing {} by {}", item, count);
+}
+
+fn list() {
+    println!("Listing current tallies.");
+}
+
+fn remove(item_id: u32) {
+    println!("Removing item {}", item_id);
 }
 
 fn main() {
     let args = Args::parse();
     match args.command {
-        Command::Add { item, count } => println!("Adding {} to {}", count, item),
+        Command::Add { item, count } => increment(item, count),
         Command::Edit { id } => println!("Editing item with ID: {}", id),
-        Command::List => println!("Listing items"),
-        Command::Remove => println!("Removing an item"),
+        Command::List => list(),
+        Command::Remove { id } => remove(id),
     }
 }
