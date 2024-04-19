14187267389b4b7fcb36b425ed9d38ffec109094
Stuart Thompson <stuart@stuartthompson.net>
2024-04-19 14:02:04 -0700

    Updates clap setup to support commands with specific sub-command arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
-use clap::{Parser,ValueEnum};
+use clap::{Parser, Subcommand};
 
-// Available commands
-#[derive(Debug, Clone, ValueEnum)]
-enum Command {
-    Add,
-    List,
-    Edit,
-    Remove,
-}
-
-// Structure describing the program arguments
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    // Command to run
-    command: Command
+    #[command(subcommand)]
+    command: Command,
+
+    /// A category for the command
+    #[arg(short, long, global = true)]
+    category: Option<String>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Adds a new item
+    Add {
+        /// The item to add
+        item: String,
+        // 
+        count: u32,
+    },
+    /// Edits an existing item
+    Edit {
+        /// The id of the item to edit
+        #[arg(short, long)]
+        id: u32,
+    },
+    /// Lists all items
+    List,
+    /// Removes an item
+    Remove,
 }
 
 fn main() {
     let args = Args::parse();
-
-    println!("Command: {:?}", args.command)
+    match args.command {
+        Command::Add { item, count } => println!("Adding {} to {}", count, item),
+        Command::Edit { id } => println!("Editing item with ID: {}", id),
+        Command::List => println!("Listing items"),
+        Command::Remove => println!("Removing an item"),
+    }
 }
